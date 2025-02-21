80baf70d8db1f666990091b3490469bedb84308e
Iwonarum <ivona.angelika@gmail.com>
2025-02-21 15:42:40 +0100

    first test done

--- demo/src/test/java/Test1.java
+++ demo/src/test/java/Test1.java
@@ -1,8 +1,27 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.List;
+import org.junit.Test;
+import org.junit.After;
+import org.junit.Before;
 
 public class Test1 {
     WebDriver driver;
 
-    
+    @Before 
+    //OTWIERANIE PRZEGLADARKI I CHROME DRIVER
+    public void driverSetup(){
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @After
+    public void driverClose(){
+        driver.quit();
+    } 
+
+    @Test
+    public void firstTest(){
+        driver.get("https://skleptest.pl/");
+    }
 }
